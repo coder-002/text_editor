0d5ecf2bbc9d58cf94065fb4b999589ab56a6e1b
coder-002 <markoshprajapati@gmail.com>
2025-08-26 14:12:16 +0545

    text editor in rust

---
+++ src/editor.rs
@@ -0,0 +1,73 @@
+use crossterm::event::Event;
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, ClearType, Clear};
+use crossterm:: execute;
+use std::io::stdout;
+
+
+pub struct Editor {
+    should_quit:bool,
+}
+
+impl Editor {
+    pub fn default()->Self{            
+        Editor{should_quit:false}            
+    }
+
+    pub fn run(&mut self){
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize()->Result<(), std::io::Error>{
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn terminate()->Result<(), std::io::Error>{
+        disable_raw_mode()
+    }
+
+    fn clear_screen()-> Result<(), std::io::Error>{
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+   
+
+    fn repl(&mut self)->Result<(), std::io::Error>{
+        loop {
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit{
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event){
+        if let Key(KeyEvent{
+            code, modifiers, ..
+        }) = event{
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL =>{
+                    self.should_quit = true;
+                }
+                _=> (),
+            }
+        }
+    }
+
+    fn refresh_screen(&self) ->Result<(), std::io::Error>{
+        if self.should_quit{
+            Self::clear_screen()?;
+            println!("Goodbye \r \n");
+        }
+        Ok(())
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
+
+
+fn main(){
+    Editor::default().run();
+    
+}
