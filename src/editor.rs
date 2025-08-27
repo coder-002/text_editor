722bdbdd4099772405fb5e062fbefd4171eafc73
coder-002 <markoshprajapati@gmail.com>
2025-08-27 13:52:50 +0545

    feat: adding tildes as vim

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,6 @@
-use crossterm::event::Event;
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, ClearType, Clear};
-use crossterm:: execute;
-use std::io::stdout;
+use crossterm::event::{read,Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+mod terminal;
+use terminal::Terminal;
 
 
 pub struct Editor {
@@ -15,36 +13,20 @@ impl Editor {
     }
 
     pub fn run(&mut self){
-        Self::initialize().unwrap();
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
     }
 
-    fn initialize()->Result<(), std::io::Error>{
-        enable_raw_mode()?;
-        Self::clear_screen()
-    }
-
-    fn terminate()->Result<(), std::io::Error>{
-        disable_raw_mode()
-    }
-
-    fn clear_screen()-> Result<(), std::io::Error>{
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
-    }
-
-   
-
     fn repl(&mut self)->Result<(), std::io::Error>{
         loop {
-            let event = read()?;
-            self.evaluate_event(&event);
             self.refresh_screen()?;
             if self.should_quit{
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event);
         }
         Ok(())
     }
@@ -64,8 +46,22 @@ impl Editor {
 
     fn refresh_screen(&self) ->Result<(), std::io::Error>{
         if self.should_quit{
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             println!("Goodbye \r \n");
+        }else{
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
+        }
+        Ok(())
+    }
+
+    fn draw_rows()-> Result<(), std::io::Error>{
+        let height = Terminal::size()?.1;
+        for current_row in 0..height{
+            print!("~");
+            if current_row + 1<height{
+                print!("\r\n");
+            }
         }
         Ok(())
     }
