16814034afed62906855d66795260d01f0fd2100
coder-002 <markoshprajapati@gmail.com>
2025-08-28 13:22:39 +0545

    feat: adding editor commang

--- src/editor/view/buffer.rs
+++ src/editor/view/buffer.rs
@@ -1,21 +1,23 @@
-use std::io::Error;
 use std::fs::read_to_string;
+use std::io::Error;
+
+use super::line::Line;
 
 #[derive(Default)]
 pub struct Buffer {
-    pub lines: Vec<String>
+    pub lines: Vec<Line>,
 }
 
-impl Buffer{
-    pub fn load(file_name: &str)-> Result<Self, Error>{
+impl Buffer {
+    pub fn load(file_name: &str) -> Result<Self, Error> {
         let contents = read_to_string(file_name)?;
         let mut lines = Vec::new();
-        for value in contents.lines(){
-            lines.push(String::from(value));
+        for value in contents.lines() {
+            lines.push(Line::from(value));
         }
         Ok(Self { lines })
     }
-    pub fn is_empty(&self)->bool{
+    pub fn is_empty(&self) -> bool {
         self.lines.is_empty()
     }
 }
